65e117479757fa8ae25f98d36fcfa8cb65ca6a84
Donatienne Borione <dborione@shi-r4-p4.s19.be>
2022-10-14 17:35:36 +0200

    update strtrim

--- Libc/ft_strtrim.c
+++ Libc/ft_strtrim.c
@@ -6,11 +6,11 @@
 /*   By: dborione <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 13:40:06 by dborione          #+#    #+#             */
-/*   Updated: 2022/10/14 16:58:53 by dborione         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:35:08 by dborione         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include <stdio.h>
+#include <stdio.h>
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -27,37 +27,27 @@ char	*ft_strtrim(char const *s1, char const *set)
 	s2 = malloc (sizeof(char) * (ft_strlen(s1) + 1));
 	if (!s2)
 		return (0);
-	while (str[i])
+	while (s1[i])
 	{
-		while (s1[i + j] == set[j])
+		if (s1[i + j] == set[j])
 		{
-			s2 = ft_substr(s1, i, ft_strlen(s));
-			j++;
-			if (str[j] == '\0')
-				j = 0;
+			i++;
+			j = 0;
 		}
-		j = 0;
-		i++;
+		j++;
 	}
-
+	// use substr here
 	s2 = &s1[i];
 	s3 = ft_strdup(s2);
 	i = ft_strlen(s1) - 1;
 	j = ft_strlen(set) - 1;
-	while (str[i])
+	while (i > 0)
 	{
-		while (s1[i + j] == set[j])
+		if (s3[i + j] == set[j])
 		{
-			s2 = ft_substr(s1, i, ft_strlen(s));
-			j++;
-			if (str[j] == '\0')
-				j = 0;
+			i--;
+			j = ft_strlen(set) - 1;
 		}
-		j = 0;
-		i++;
-	}while (i > 0 && s1[i] == set[j])
-	{
-		i--; 
 		j--;
 	}
 	((char *)s3)[i] = '\0';
@@ -71,4 +61,4 @@ int	main()
 
 	printf("%s\n", ft_strtrim(s1, set));
 	return (0);
-}*/
+}
